dff95f487904ed2df3084a777f1346881c715396
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-08 19:53:38 +1000

    set up project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="section">
+        <div className="title">
+          <Loading />
+          <h2>Our Tours</h2>
+        </div>
+
+        <div className="allArticles">
+          <article className="article">
+            <img className="img"></img>
+            <div className="footerDiv">
+              <div className="articleDesc">
+                <p>Best of Paris In 7 Days Tour</p>
+                <p>$1,995</p>
+              </div>
+              <p></p>
+              <button className="btn">Not interested</button>
+            </div>
+          </article>
+        </div>
+      </section>
+    </main>
   );
 }
 
